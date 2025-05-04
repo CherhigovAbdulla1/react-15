13cc725c0779d6eda610f88f616f2a42077a663c
Abdulla <abdullaborz96@gmail.com>
2025-05-04 15:56:25 +0300

    запускаеться с npm start

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,13 +1,16 @@
 import Buttons from '../assets/images/buttons.svg'
-
-function Buttonslogo(){ 
+ 
+ function Buttonslogo(props){ 
+    
+    const {plus, minus, reset} = props;
+ 
     return(
-        <div className='counter-buttons'>
-         <button className="button green-button">+</button>
-        <button className="button reset-button"><img src={Buttons} alt="button"></img></button>
-        <button className="button red-button">-</button>
+         
+        <div className='counter-buttons'>           
+         <button className="button green-button" onClick={plus}>+</button>
+        <button className="button reset-button" onClick={reset}><img src={Buttons} alt="button"></img></button>
+        <button className="button red-button" onClick={minus}>-</button>
         </div>
-      
     )
 }
 
--- src/components/Number.jsx
+++ src/components/Number.jsx
@@ -1,8 +1,28 @@
+import { useState } from "react"
+import Buttonslogo from "./Buttons"
+
 
 function Number (){
+    const [counter, setCounter] = useState(0)
+
+    const plus = () => {
+        setCounter(counter + 1);
+      };
+    
+      const minus = () => {
+        setCounter(counter - 1);
+      };
+    
+      const reset = () => {
+        setCounter(0);
+      };
+
+      
     return(
         <div className="numberValue">
-            <h1>0</h1>
+            <h1>{counter}</h1>
+
+            <Buttonslogo plus={plus} minus={minus} reset={reset} />
         </div>
     )
 }
