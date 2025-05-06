b6004229931fd155632e0357318bb83e3128494b
Abdulla <abdullaborz96@gmail.com>
2025-05-06 15:25:33 +0300

    сделал дз

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,72 @@
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
  
-function App(){
 
-    return(
-       <div className="container"> 
-         <Header/>
-         <MainContent/>
-         <Footer/>
-       </div> 
-      
-    );
+
+       //1
+const array = [22, 0, -45, 1001, 8, -6, -0.1, 101, 2.5, -55555];
+
+const positive = array.filter(num => num > 0);
+
+console.log(positive);
+
+     //2
+
+const messages = [
+  { message: 'hello', error: true },
+  { message: 'javascript', error: false },
+  { message: 'expovisiov', error: true },
+  { message: 'react', error: true },
+  { message: 'angular', error: false },
+  { message: 'es6', error: false },
+];
+
+const filterMessages = messages.filter(message => message.error === false);
+
+console.log(filterMessages);
+
+
+   //3
+
+
+const words = ['экспо', 'js', 'react', 'css', 'angular', 'vue', 'html'];
+
+const newWords = words.map(word => (word.length < 5 ? '*' : word));
+
+console.log(newWords);
+
+
+
+         //4
+const sendMessage = (message) => {
+ 
+  console.log(`sendMessage: ${message}`); 
+};
+
+const half = (num ) => num  / 2;
+
+const showConsole = () => console.log('Экспо');
+
+const concatWords = (first, second) => first + second;
+                
+           //5
+
+function justText() {
+  return 'expo';
 }
 
-export default App; 
+function logging(text) {
+  console.log(text);
+}
+
+function add(x, y) {
+  const z = 3;
+
+  return z * x * y;
+}
+
+function onlyPositive(num) {
+  if (num < 0) {
+    return false;
+  }
+
+  return true;
+}
